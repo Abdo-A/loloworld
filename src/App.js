533d1a059548a6b89bfbd0b55ff79b9e802f07c4
Abdo-A <abdopoetry@gmail.com>
2018-05-29 03:05:52 +0200

    added Unirest to fetch mashable api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+const unirest = require("unirest");
 
 class App extends Component {
+  componentWillMount() {
+    unirest
+      .get("https://simple-weather.p.mashape.com/aqi?lat=30.0355&lng=31.223")
+      .header(
+        "X-Mashape-Key",
+        "J3YJWUlCN1mshIPwlMb5ilplW4qYp1Lz9x1jsnM6OO3OiTushH"
+      )
+      .header("Accept", "text/plain")
+      .end(function(result) {
+        console.log(result.status, result.headers, result.body);
+      });
+  }
+
   render() {
     return (
       <div className="App">
